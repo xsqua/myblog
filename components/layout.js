a61ad559e2c139710f08ccb1d6c67c3143acd2d4
xsqua <xsqua@protonmail.com>
2020-06-29 11:45:39 +0800

    新文件：   components/layout.js

---
+++ components/layout.js
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
+const name='夏学进'
+export const siteTitle = '这是夏学进的博客网站'
+
+export default function Layout({ children,home }) {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<meta name="description" content="来自中国" />
+				<meta name="og:title" content={siteTitle} />
+			</Head>
+			<header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+		<main>{children}</main>
+		{!home && (
+			<div className={styles.backToHome}>
+			<Link href="/">
+				<a>← Back to home</a>
+			</Link>
+			</div>
+		)}
+		</div>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import Link from 'next/link'
+import Layout,{siteTitle} from '../components/layout'
 
-export default function Home() {
-  return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
-    </div>
-  )
+import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css'
+
+export	default function Home (){
+	return (
+		<Layout home>
+				<Head>
+						<title>{siteTitle}</title>
+				</Head>
+				<section className={utilStyles.headingMd}>
+					<p>个人文档</p>
+					<p>这是一个模拟站</p>
+				</section>
+		</Layout>
+	)
 }
