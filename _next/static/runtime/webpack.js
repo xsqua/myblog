908766d9ae36c68100224929a3316e2f380dd565
xsqua <xsqua@protonmail.com>
2020-06-30 11:23:41 +0800

    Deploy Next.js to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4e12582a2a43fb1b7b40";
+/******/ 	var hotCurrentHash = "6af0e2aab7f708a085b3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
