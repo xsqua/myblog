aa5b11bdbeeca33736f98b8a524568e3558b6d3a
xsqua <xsqua@protonmail.com>
2020-07-01 08:59:59 +0800

    static

--- components/layout.js
+++ components/layout.js
@@ -17,7 +17,7 @@ export default function Layout({ children,home }) {
         {home ? (
           <>
             <img
-              src="/myblog/images/profile.jpg"
+              src="/static/images/profile.jpg"
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
             />
@@ -28,7 +28,7 @@ export default function Layout({ children,home }) {
             <Link href="/">
               <a>
                 <img
-                  src="/images/profile.jpg"
+                  src="/static/images/profile.jpg"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
